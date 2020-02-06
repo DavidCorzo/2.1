abb8283107f8cb502dadff92eda3e15c996ff459
DavidCorzo <davidcorzo@ufm.edu>
2020-02-06 09:03:44 -0600

    automatic

--- autogit.ps1
+++ autogit.ps1
@@ -2,8 +2,8 @@ $path = Get-Location
 Write-Host $("CWD ======> {0}" -f $path)
 Resolve-Path -LiteralPath 'D:\one\one'
 Write-Host "======> Initialize! dgcm "
-[string] $date = Get-Date - DisplayHint 
-$date = $date -replace ":", "-"
+[string] $date = Get-Date -Format " dddd yyyy-MM-dd HH-mm-ss K"
+$date = $date.Replace("(:)", "-")
 Write-Host $("{0}" -f $date)
 [string] $Message = $( Read-Host "Input commit message ") 
 Write-Host ("=====> {0} ==> {1}" -f $date, $Message)
@@ -15,10 +15,11 @@ powershell.exe /c "git add ."
 Write-Host "=====> git add ."
 powershell.exe /c "git status"
 Write-Host "=====> git status"
-powershell.exe /c ('git commit -m "{0}  {1}"' -f $date, $Message)
-Write-Host ('=====> git commit -m "{0}  {1}"' -f $date, $Message)
-# powershell.exe /c "git push -u origin master"
-# Write-Host "=====> git push -u origin master"
 powershell.exe /c "git remote -v"
 Write-Host "=====> git remote -v"
+powershell.exe /c ('git commit -m "{0}  {1}"' -f $date, $Message)
+Write-Host ('=====> git commit -m "{0}  {1}"' -f $date, $Message)
+powershell.exe /c "git push origin master"
+Write-Host "=====> git push origin master"
+
 
