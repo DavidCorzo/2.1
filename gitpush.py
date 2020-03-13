7ab60bc03e34b730669a47ebaf319bf0dfe37a60
DavidCorzo <davidcorzo@ufm.edu>
2020-03-12 19:47:54 -0600

    automatic commit

--- gitpush.py
+++ gitpush.py
@@ -7,10 +7,15 @@ COMMIT_MESSAGE = 'automatic commit'
 def git_push():
     try:
         repo = Repo(PATH_OF_GIT_REPO)
+        print(1)
         repo.git.add(update=True)
+        print(2)
         repo.index.commit(COMMIT_MESSAGE)
+        print(3)
         origin = repo.remote(name='origin')
+        print(4)
         origin.push()
+        print(5)
     except:
         print('Some error occured while pushing the code')    
 
