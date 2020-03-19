2d05c245ac6ffad6c20d04a210b11cb12a86a462
DavidCorzo <davidcorzo@ufm.edu>
2020-03-19 08:49:02 -0600

    08:49:00 @ teorias_eticas

--- autogitpull_u.py
+++
@@ -1,9 +0,0 @@
-import dependency_functions,time
-
-if __name__ == "__main__":
-    try:
-        print(dependency_functions.git_pull())
-        print("-"*70 + "Done" + "-"*70)
-    except Exception as e:
-        print(e)
-        time.sleep(10)
--- autogitpush_u.py
+++
@@ -1,9 +0,0 @@
-import dependency_functions,time
-
-if __name__ == "__main__":
-    try:
-        print("\n",dependency_functions.git_push(dependency_functions.get_message()))
-        print("-"*20 + "Done" + "-"*20)
-    except Exception as e:
-        print(e)
-        time.sleep(10)
